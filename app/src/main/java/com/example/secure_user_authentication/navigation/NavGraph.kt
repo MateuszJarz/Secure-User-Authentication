8319b5d2e433620ffda96a9b4800167565844f1d
Mat <62247805+MateuszKapelusz@users.noreply.github.com>
2022-05-08 14:29:16 +0200

    setup navigate compose

---
+++ app/src/main/java/com/example/secure_user_authentication/navigation/NavGraph.kt
@@ -0,0 +1,22 @@
+package com.example.secure_user_authentication.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Login.route
+    ) {
+        composable(route = Screen.Login.route) {
+
+        }
+        composable(route = Screen.Profile.route) {
+
+        }
+    }
+
+}
