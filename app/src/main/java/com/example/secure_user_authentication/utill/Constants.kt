2cdabaac09b922ba9eaaac61900b7c36e900483b
Mat <62247805+MateuszKapelusz@users.noreply.github.com>
2022-05-15 18:47:21 +0200

    create signIn/signUp,StartActivityForResult fun

--- app/src/main/java/com/example/secure_user_authentication/utill/Constants.kt
+++ app/src/main/java/com/example/secure_user_authentication/utill/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
 
     const val PREFERENCES_SINGED_IN_KEY = "signed_in_key"
     const val PREFERENCES_NAME = "app_preferences"
+
+    const val CLIENT_ID = "493442449096-s3f50h9miaqpdqjh9n95u1p900t618t0.apps.googleusercontent.com"
 }
